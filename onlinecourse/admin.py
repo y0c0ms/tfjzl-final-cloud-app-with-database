4a32fd4533bcc7131def0ac03adc6f53cef165e7
y0c0ms <majpr@iscte-iul.pt>
2025-07-16 15:38:36 +0100

    Implement complete exam functionality - Add exam questions and answers to course detail page - Implement submission evaluation with scoring - Add exam result page with detailed feedback - Configure admin interface for questions and choices - Add database migrations and static files

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
 
@@ -9,18 +9,32 @@ class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2
 
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
-    inlines = [LessonInline]
+    inlines = [LessonInline, QuestionInline]
     list_display = ('name', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
 
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ['content', 'course', 'grade']
+    list_filter = ['course']
 
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ['content', 'question', 'is_correct']
+    list_filter = ['question', 'is_correct']
 
 # <HINT> Register Question and Choice models here
 
@@ -28,3 +42,6 @@ admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
+admin.site.register(Submission)
